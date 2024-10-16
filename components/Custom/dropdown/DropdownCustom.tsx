1f4cd01db21a1abbb2d50bab0ebd8a51295ceb4d
ArthurSapore <arthursapore17@gmail.com>
2024-10-15 22:31:36 -0300

    changes in ModeToogle, Dropdown and NavigationMenu

--- components/Custom/dropdown/DropdownCustom.tsx
+++ components/Custom/dropdown/DropdownCustom.tsx
@@ -1,19 +1,4 @@
 import {
-   Cloud,
-   CreditCard,
-   Github,
-   Keyboard,
-   LifeBuoy,
-   LogOut,
-   Mail,
-   MessageSquare,
-   Plus,
-   PlusCircle,
-   Settings,
-   User,
-   UserPlus
-} from 'lucide-react'
-import {
    DropdownMenu,
    DropdownMenuContent,
    DropdownMenuItem,
@@ -49,92 +34,6 @@ interface Menu {
    items: MenuItem[]
 }
 
-export const menuItems: MenuItem[] = [
-   {
-      label: 'Profile',
-      icon: <User className="mr-2 h-4 w-4" />,
-      shortcut: '⇧⌘P',
-      onClick: () => console.log('Profile clicked')
-   },
-   {
-      label: 'Billing',
-      icon: <CreditCard className="mr-2 h-4 w-4" />,
-      shortcut: '⌘B',
-      onClick: () => console.log('Billing clicked')
-   },
-   {
-      label: 'Settings',
-      icon: <Settings className="mr-2 h-4 w-4" />,
-      shortcut: '⌘S',
-      onClick: () => console.log('Settings clicked')
-   },
-   {
-      label: 'Keyboard shortcuts',
-      icon: <Keyboard className="mr-2 h-4 w-4" />,
-      shortcut: '⌘K',
-      onClick: () => console.log('Keyboard shortcuts clicked')
-   },
-   {
-      separator: true // Isso adiciona um separador
-   },
-   {
-      label: 'Invite users',
-      icon: <UserPlus className="mr-2 h-4 w-4" />,
-      subItems: [
-         {
-            label: 'Email',
-            icon: <Mail className="mr-2 h-4 w-4" />,
-            onClick: () => console.log('Email clicked')
-         },
-         {
-            label: 'Message',
-            icon: <MessageSquare className="mr-2 h-4 w-4" />,
-            onClick: () => console.log('Message clicked')
-         },
-         {
-            separator: true
-         },
-         {
-            label: 'More...',
-            icon: <PlusCircle className="mr-2 h-4 w-4" />,
-            onClick: () => console.log('More clicked')
-         }
-      ]
-   },
-   {
-      label: 'New Team',
-      icon: <Plus className="mr-2 h-4 w-4" />,
-      shortcut: '⌘+T',
-      onClick: () => console.log('New Team clicked')
-   },
-   {
-      separator: true
-   },
-   {
-      label: 'GitHub',
-      icon: <Github className="mr-2 h-4 w-4" />,
-      onClick: () => console.log('GitHub clicked')
-   },
-   {
-      label: 'Support',
-      icon: <LifeBuoy className="mr-2 h-4 w-4" />,
-      onClick: () => console.log('Support clicked')
-   },
-   {
-      label: 'API',
-      icon: <Cloud className="mr-2 h-4 w-4" />,
-      disabled: true
-   },
-   {
-      separator: true
-   },
-   {
-      label: 'Log out',
-      icon: <LogOut className="mr-2 h-4 w-4" />,
-      shortcut: '⇧⌘Q',
-      onClick: () => console.log('Log out clicked')
-   }
-]
 // Componente para renderizar um item de menu
 const DropdownMenuItemCustom = ({ item }: { item: MenuItem }) => {
    return (
